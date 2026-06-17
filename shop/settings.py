0de5cb5faf41b51e18a9268b95665a2899575034
Erik Surala <d23836@oa-opava.cz>
2026-06-18 01:53:21 +0200

    zmena mysql na sqlite

--- shop/settings.py
+++ shop/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ErikSurala$eshop',
-        'USER': 'ErikSurala',
-        'PASSWORD': '1',
-        'HOST': 'eriksurala.mysql.eu.pythonanywhere-services.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
