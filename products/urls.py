bb17bbe5163c948d44637f2fada2ac1c9a10f9c0
Erik Surala <d23836@oa-opava.cz>
2026-06-18 02:13:06 +0200

    uprava webu, pridani stranek na login a na zobrazeni objednavek

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.prihlaseni, name='login'),
+    path('logout/', views.odhlaseni, name='logout'),
+    path('moje-objednavky/', views.moje_objednavky, name='moje_objednavky'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-from django.http import HttpResponse
+from .models import Produkt, Objednavka
+
 
 def index(request):
-    return HttpResponse("Vítejte v e-shopu")
+    produkty = Produkt.objects.all()
+    return render(request, 'products/index.html', {'produkty': produkty})
+
+
+def prihlaseni(request):
+    chyba = ""
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('moje_objednavky')
+        else:
+            chyba = "Špatné jméno nebo heslo"
+
+    return render(request, 'products/login.html', {'chyba': chyba})
+
+
+def odhlaseni(request):
+    logout(request)
+    return redirect('index')
+
+
+@login_required
+def moje_objednavky(request):
+    objednavky = Objednavka.objects.all()
+    return render(request, 'products/moje_objednavky.html', {'objednavky': objednavky})
